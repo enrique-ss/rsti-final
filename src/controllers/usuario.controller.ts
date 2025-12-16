fa6c09a2758e4c050f33fcfb4c79b92e62800ab0
gabgoooooool <gabrielsoaresdias81@gmail.com>
2025-12-16 17:25:31 -0300

    criarDemandas adicionado

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -3,5 +3,5 @@ import { db } from "../database/connection";
 export const criarUsuario = async (req: any, res: any) => {
     const { nome, email, senha, telefone, nivel_acesso } = req.body;
     const usuario = await db('usuario').insert({ nome, email, senha, telefone, nivel_acesso });
-    return res.json(usuario)
+    return res.json(usuario);
 };
